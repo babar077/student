6329011c8b89e15007851f8c7f1a897774559d2c
babar <babar@ubuntu.(none)>
2014-06-26 01:26:51 -0700

    student

---
+++ student.rb
@@ -0,0 +1,19 @@
+print "what's your first name?"
+first_name=gets.chomp
+first_name
+
+print "what's your rollnum?"
+rollnum=gets.chomp
+rollnum
+
+print "what's your Emailaddress?"
+emailaddress_name=gets.chomp
+emailaddress_name
+
+print "what's your Sex?"
+sex_name=gets.chomp
+sex_name
+
+puts "Name:#{first_name} Rollnum:#{rollnum} Emailaddress:#{emailaddress_name} sex:#{sex_name}"
+
+
---
+++ student1.rb
@@ -0,0 +1,28 @@
+
+
+class Student
+ def initialize(first_name,rollnum,emailaddress,sex)
+@first_name = first_name
+@rollnum = rollnum
+@emailaddress = emailaddress
+@sex = sex
+ end
+
+student = Student.new("#{first_name}","#{rollnum}","#{emailaddress}","#{sex}")
+print student
+def register(student)
+student =register
+
+end
+ def update(student)
+    puts "#{student}"
+  end
+ def delete(student)
+ student.delete()
+ end
+ def display
+puts  "#{@student}"
+end
+end
+
+
